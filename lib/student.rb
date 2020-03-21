c6b951ee10ac7d7d8f7cfb0035f934d66aaff8c4
Daniel Sun <danielsun12345@gmail.com>
2020-03-21 00:08:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,11 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
+<<<<<<< HEAD
   def initialize(name, grade, id = nil)
+=======
+  def initialize(name, grade, id=nil)
+>>>>>>> 1a45b0fcb2e569805f1a7312c5f7fbb061034eb0
     @name = name 
     @grade = grade 
     @id = id 
@@ -28,11 +32,19 @@ class Student
   end 
   
   def save
+<<<<<<< HEAD
     sql = <<-SQL 
       INSERT INTO students (name, grade) VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+=======
+      sql = <<-SQL
+  insert into students (name,grade) values(?,?);
+  SQL
+  DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
+>>>>>>> 1a45b0fcb2e569805f1a7312c5f7fbb061034eb0
   end 
   
   def self.create(name:, grade:)
